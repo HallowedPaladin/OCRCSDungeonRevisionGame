eff838fe7a73f89d90181e7738dab10306bb7a44
HallowedPaladin <lukezgwilliams@gmail.com>
2024-04-11 17:44:19 +0100

    Initial skeleton
    Completed skeleton of project
    - Classroom and prefences buttons now bring user to respective screens
    - Pages reordered
    - Login, Register, and forgotten password scenes added

---
+++ Insignia/Assets/Scripts/TitleScreen/Quit.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class QuitManager : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+}
+
